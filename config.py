43479f5691165c83131fdfc811c44eab58ada7d3
Bella Amandine <bellamandy45@gmail.com>
2020-12-01 13:20:36 +0200

    Add some configurations

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    pass
+
+class ProdConfig(Config):
+    '''
+    Production configuration
+    Args:
+        Config: the parent configuration class
+    '''
+
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+    Args:
+        Config: the parent configuration class
+    '''
+
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production' : ProdConfig
+}
+
