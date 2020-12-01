e4b7dd1147c853e03845f293b10e2d7550f34bd2
Bella Amandine <bellamandy45@gmail.com>
2020-12-01 15:56:48 +0200

    Define pitch model

--- app/models.py
+++ app/models.py
@@ -1,1 +1,10 @@
-       
+from . import db     
+
+class pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    project_name = db.Column(db.String(255))
+    pitch_description = db.Column(db.String(255))
+
+    def __repr__(self):
+        return f'Pitch {self.project_name}'
