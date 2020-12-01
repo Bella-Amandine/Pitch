48bd29a7ee7e4b9f1d3258a1d97835331e6013eb
Bella Amandine <bellamandy45@gmail.com>
2020-12-01 13:33:38 +0200

    Initialise the flask application

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask 
+from config import config_options
+
+#Initializing Flask extension
+
+
+def create_app(config_name):
+
+    #Initializing application
+    app = Flask(__name__)
+
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    #Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    return app
