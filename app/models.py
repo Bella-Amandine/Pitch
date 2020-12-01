c3f61ae6517bc4f2dc1294e8436cf08fda68f9da
Bella Amandine <bellamandy45@gmail.com>
2020-12-01 16:28:55 +0200

    implement first migration

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from . import db     
 
-class pitch(db.Model):
+class Pitch(db.Model):
     __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key = True)
     project_name = db.Column(db.String(255))
